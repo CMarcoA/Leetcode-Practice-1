477654412a3bead66504f6e54a0955b9280e9319
Claudius Marco Andrew <claudiusma2022@gmail.com>
2025-07-15 13:23:28 -0500

    AlgoMonster - Python

---
+++ LargestLocalValuesMatrix.java
@@ -0,0 +1,38 @@
+// 2373. Largest Local Values in a Matrix
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// You are given an n x n integer matrix grid.
+
+// Generate an integer matrix maxLocal of size (n - 2) x (n - 2) such that:
+
+// maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid centered around row i + 1 and column j + 1.
+// In other words, we want to find the largest value in every contiguous 3 x 3 matrix in grid.
+
+// Return the generated matrix.
+
+// Input: grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]
+// Output: [[9,9],[8,6]]
+// Explanation: The diagram above shows the original matrix and the generated matrix.
+// Notice that each value in the generated matrix corresponds to the largest value of a contiguous 3 x 3 matrix in grid.
+
+class Solution {
+    public int[][] largestLocal(int[][] grid) {
+        int n = grid.length;
+        int[][] maxLocal = new int[n - 2][n - 2];
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                int max = Integer.MIN_VALUE;
+                for (int x = 0; x < 3; x++) {
+                    for (int y = 0; y < 3; y++) {
+                        max = Math.max(max, grid[i + x][j + y]);
+                    }
+                }
+                maxLocal[i][j] = max;
+            }
+        }
+        return maxLocal;
+    }
+}
